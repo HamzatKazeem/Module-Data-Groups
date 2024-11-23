00d5cb96d328a7d70ed43da362ec65bfcba5e5d7
Kazeem Hamzat <hamzatkazeem@yahoo.com>
2024-11-23 15:05:59 +0000

    Completed the debug section
    Completed the 3 questions in the debug section

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -11,6 +11,12 @@ const author = {
   alive: true,
 };
 
+console.log(author)
+/*
 for (const value of author) {
   console.log(value);
 }
+  */
+// Explanation: Javascript object are not iterable unless they implement the iterable protocol such as arrey.
+// Therefore, we cannot use for stmt as expressed in line 16. We can only use Object.keys or Object.entries, to iterate over the properties or entries of an object.
+
--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -10,6 +10,12 @@ const recipe = {
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
-console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+console.log(`${recipe.title} serves ${recipe.serves}`)
+for (const ingredient of recipe.ingredients) {
+  console.log(ingredient);
+}
+console.log(inc)
+//${recipe}`);
+//Explanation: The is expect to extract values from the object recipe and substitute them to their 
+// respective placeholders within the console.log stmt in line 13
+// For loop stmt has been used to iterate object attributes ingredients to lis its array members.
